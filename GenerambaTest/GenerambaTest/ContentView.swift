330500d876ae0f9b4c735a28d1a714f16bfb38d3
yohei <>
2024-12-29 02:00:13 +0900

    add project

---
+++ GenerambaTest/GenerambaTest/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  GenerambaTest
+//
+//  Created by yohei on 2024/12/29.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ GenerambaTest/GenerambaTest/GenerambaTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  GenerambaTestApp.swift
+//  GenerambaTest
+//
+//  Created by yohei on 2024/12/29.
+//
+
+import SwiftUI
+
+@main
+struct GenerambaTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
